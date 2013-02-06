fdd61f05b0eb70992acaea7150ba153f5368499e
Glenn Jorde <glenn@tregusti.com>
2013-02-06 14:45:51 +0100

    Make file clickable in msvs when error.

--- Goose/Core/Output/OutputService.cs
+++ Goose/Core/Output/OutputService.cs
@@ -1,63 +1,63 @@
 ﻿namespace Goose.Core.Output
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using Microsoft.VisualStudio.Shell.Interop;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using Microsoft.VisualStudio.Shell.Interop;
 
-    public class OutputService
-        : IOutputService
-    {
+	public class OutputService
+		: IOutputService
+	{
 		private readonly IVsOutputWindow outputWindow;
 		private IDictionary<string, IVsOutputWindowPane> panes = new Dictionary<string, IVsOutputWindowPane>();
-        
+
 		public OutputService(IServiceProvider serviceProvider)
 		{
 			this.outputWindow = serviceProvider.GetService(typeof(SVsOutputWindow)) as IVsOutputWindow;
 		}
 
-        public void Handle(CommandOutput output)
+		public void Handle(CommandOutput output)
 		{
-            if (output.Version == 1)
+			if (output.Version == 1)
 			{
-                var pane = GetPane(output.Name);
-                pane.Clear();
-                if (output.Time.HasValue)
+				var pane = GetPane(output.Name);
+				pane.Clear();
+				if (output.Time.HasValue)
 				{
-                    pane.OutputString("\nInvoked @ " + output.Time.Value.ToString("s") + ":\n");
+					pane.OutputString("\nInvoked @ " + output.Time.Value.ToString("s") + ":\n");
 				}
 
-                foreach (var item in output.Results)
+				foreach (var item in output.Results)
 				{
-				    if (item.Type == CommandOutputItemType.Error)
-				    {				        
-				        var outputText = CreateErrorOutput(item);
-                        pane.OutputTaskItemString(outputText + Environment.NewLine, VSTASKPRIORITY.TP_NORMAL, VSTASKCATEGORY.CAT_CODESENSE, "", 0, item.FileName ?? "", item.Line, string.Format("{0}: {1}", item.Message, outputText));
-				    }
-				    if (item.Type == CommandOutputItemType.Message)
-				    {
-                        pane.OutputString(item.Message + Environment.NewLine);
-				    }
+					if (item.Type == CommandOutputItemType.Error)
+					{
+						var outputText = CreateErrorOutput(item);
+						pane.OutputTaskItemString(outputText + Environment.NewLine, VSTASKPRIORITY.TP_NORMAL, VSTASKCATEGORY.CAT_CODESENSE, "", 0, item.FullPath ?? item.FileName ?? "", item.Line, string.Format("{0}: {1}", item.Message, outputText));
+					}
+					if (item.Type == CommandOutputItemType.Message)
+					{
+						pane.OutputString(item.Message + Environment.NewLine);
+					}
 				}
-			    
+
 				pane.FlushToTaskList();
 			}
 		}
 
-        private static string CreateErrorOutput(CommandOutputItem item)
-        {
-            var outputText = String.Format("{0} #{1}: {2}", item.FileName, item.Line, item.Message);
-            if (!String.IsNullOrWhiteSpace(item.Excerpt))
-            {
-                var excerpt = from line in item.Excerpt.Split('\n')
-                              select "  " + line.Trim();
+		private static string CreateErrorOutput(CommandOutputItem item)
+		{
+			var outputText = String.Format("{0} #{1}: {2}", item.FileName, item.Line, item.Message);
+			if (!String.IsNullOrWhiteSpace(item.Excerpt))
+			{
+				var excerpt = from line in item.Excerpt.Split('\n')
+							  select "  " + line.Trim();
 
-                outputText += ":" + Environment.NewLine + String.Join("\n", excerpt);
-            }
-            return outputText;
-        }
+				outputText += ":" + Environment.NewLine + String.Join("\n", excerpt);
+			}
+			return outputText;
+		}
 
-        private IVsOutputWindowPane GetPane(string name)
+		private IVsOutputWindowPane GetPane(string name)
 		{
 			if (!panes.ContainsKey(name))
 			{
@@ -68,6 +68,6 @@
 				panes[name] = pane;
 			}
 			return panes[name];
-		}  
+		}
 	}
 }
