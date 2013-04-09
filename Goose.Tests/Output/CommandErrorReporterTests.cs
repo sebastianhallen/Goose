e03f737ac08f832d97a93d9810a2f2e2377594af
Sebastian Hallén <sebastian.hallen@gmail.com>
2013-04-09 23:09:20 +0200

    starting with new error handler

---
+++ Goose.Tests/Output/CommandErrorReporterTests.cs
@@ -0,0 +1,57 @@
+﻿namespace Goose.Tests.Output
+{
+    using System.Linq;
+    using FakeItEasy;
+    using Goose.Core.Action;
+    using Goose.Core.Action.PowerShell;
+    using Goose.Core.Output;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CommandErrorReporterTests
+    {
+        [UnderTest] private CommandErrorReporter errorReporter;
+        [Fake] private IErrorTaskHandler errorTaskHandler;
+        [Fake] private ICommandLogParser logParser;
+
+        [SetUp]
+        public void Before()
+        {
+            Fake.InitializeFixture(this);
+        }
+
+        [Test]
+        public void Should_send_error_task_to_error_task_handler_when_reporting_error()
+        {
+            var commandOutput = this.BuildCommandOutput("some error");
+            A.CallTo(() => this.logParser.Parse(A<string>._)).Returns(commandOutput);
+
+            this.errorReporter.Report(A.Fake<ShellCommand>(), A.Fake<CommandResult>());
+
+            A.CallTo(() => this.errorTaskHandler.Add(A<GooseErrorTask>._)).MustHaveHappened();
+        }
+
+        [Test]
+        public void Should_parse_command_result_when_creating_error_task()
+        {
+            var result = new CommandResult("build log", null, null);
+            
+            this.errorReporter.Report(A.Fake<ShellCommand>(), result);
+
+            A.CallTo(() => this.logParser.Parse("build log")).MustHaveHappened();
+        }
+
+        private CommandOutput BuildCommandOutput(params string[] messages)
+        {
+            var errors = messages.Select(message => new CommandOutputItem
+                {
+                    Type = CommandOutputItemType.Error,
+                    Message = message
+                });
+            return new CommandOutput
+                {
+                    Results = errors.ToList()
+                };
+        }
+    }
+}
---
+++ Goose/Core/Output/CommandErrorReporter.cs
@@ -0,0 +1,57 @@
+﻿namespace Goose.Core.Output
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Goose.Core.Action;
+    using Goose.Core.Action.PowerShell;
+    using Microsoft.VisualStudio.Shell;
+
+    public class CommandErrorReporter
+    {
+        private readonly IErrorTaskHandler errorTaskHandler;
+        private readonly ICommandLogParser logParser;
+
+        public CommandErrorReporter(IErrorTaskHandler errorTaskHandler, ICommandLogParser logParser)
+        {
+            this.errorTaskHandler = errorTaskHandler;
+            this.logParser = logParser;
+        }
+
+        public void Report(ShellCommand command, CommandResult result)
+        {
+            var resultTasks = this.BuildErrorTask(command, result.Result);
+
+            foreach (var errorTask in resultTasks)
+            {
+                this.errorTaskHandler.Add(errorTask);
+            }
+        }
+
+        private IEnumerable<GooseErrorTask> BuildErrorTask(ShellCommand command, string content)
+        {
+            var output = this.logParser.Parse(content);
+
+            return output.Results
+                         .Where(error => error.Type.Equals(CommandOutputItemType.Error))
+                         .Select(error => new GooseErrorTask(command, error));
+        }
+    }
+
+    public interface IErrorTaskHandler
+    {
+        void Add(GooseErrorTask task);
+    }
+
+    public class GooseErrorTask
+        : ErrorTask
+    {
+        private readonly ShellCommand command;
+        private readonly CommandOutputItem error;
+
+        public GooseErrorTask(ShellCommand command, CommandOutputItem error)
+        {
+            this.command = command;
+            this.error = error;
+        }
+    }
+}
